b652805f598924f267186b60972915ca068897ad
kasparmartens <kaspar.martens@gmail.com>
2016-01-21 11:00:34 +0000

    NIPS dataset and how to read it in

---
+++ data_import.R
@@ -0,0 +1,11 @@
+# read in vocabulary and data
+voc = read.table("vocab.nips.txt", header=FALSE)
+docs = read.table("docword.nips.txt", skip=3)
+# number of documents
+n_docs = max(docs$V1)
+# dictionary size
+n_words = nrow(voc)
+
+# create a "document x words" matrix
+W = matrix(0, n_docs, n_words)
+W[cbind(docs$V1, docs$V2)] = docs$V3
